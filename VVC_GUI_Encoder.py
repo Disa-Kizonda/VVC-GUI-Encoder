c1774a15fe8a14d91e3ea0cb3b4f0195a87d86ca
DisaKzionda <53359732+DisaKzionda@users.noreply.github.com>
2022-09-15 08:59:56 +1000

    2-pass is fixed

--- VVC_GUI_Encoder.py
+++ VVC_GUI_Encoder.py
@@ -27,19 +27,20 @@ def SelectButton():
 def EncodeButton():
 	global ii
 	os.system('ffmpeg_vvceasy.exe -i "'+filename+'" -q:a 0 -map a temp.wav')
-	os.system("exhale.exe c temp.wav temp.m4a") 
+	os.system('exhale.exe c temp.wav temp.m4a') 
 	if passes.get() == "1 pass": 
 		os.system('ffmpeg_vvceasy.exe -y -i "'+filename+'" -pix_fmt yuv420p -f yuv4mpegpipe - | vvencapp.exe --y4m -i - --preset '+preset.get()+' -q '+quality.get()+' -o temp.266')
 	if passes.get() == "2 pass": 
-		os.system('ffmpeg_vvceasy.exe -y -i "'+filename+'" -pix_fmt yuv420p -f yuv4mpegpipe - | vvencapp.exe --y4m -i - --preset '+preset.get()+' --qpa 1 -p 2 -b '+qualitytwo.get()+'k -o temp.266')
+		os.system('ffmpeg_vvceasy.exe -y -i "'+filename+'" -pix_fmt yuv420p temp.y4m')
+		os.system('vvencapp.exe --y4m -i temp.y4m --preset '+preset.get()+' --qpa 1 -p 2 -b '+qualitytwo.get()+'k -o temp.266')
 	if os.path.exists('temp.wav'):
-		os.remove("temp.wav")
-		os.system("mp4box.exe -add temp.266:fmt=VVC -add temp.m4a -new "+'"'+saveto.get()+'"')
-		os.remove("temp.m4a")
+		os.remove('temp.wav')
+		os.system('mp4box.exe -add temp.266:fmt=VVC -add temp.m4a -new "'+saveto.get()+'"')
+		os.remove('temp.m4a')
 	else:
 		os.system("mp4box.exe -add temp.266:fmt=VVC -new "+'"'+saveto.get()+'"')
 	os.remove("temp.266")
-	os.system("ffmpeg_vvceasy.exe -y -i "+'"'+saveto.get()+'"'+" -vf 'thumbnail' -frames:v 1  temp.jpg")
+	os.system('ffmpeg_vvceasy.exe -y -i "'+saveto.get()+'" -vf "thumbnail" -frames:v 1 temp.jpg')
 	os.system("mp4box.exe -add "+'"'+saveto.get()+'"'+" -add temp.jpg -new "+'"'+saveto.get()+'"')
 	imgtwo=Image.open('temp.jpg')
 	if imgtwo.size[0] >= imgtwo.size[1]:
